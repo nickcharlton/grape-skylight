443e85a993939a3f4e438b31d05ead51f659c7d3
Nick Charlton <nick@nickcharlton.net>
2015-03-25 15:44:29 +0000

    Fixes named route issues with Skylight.
    This follows along what's done by Skylight with Sinatra and sets the route name
    on the current instrumenter instance.

--- lib/grape-skylight/middleware.rb
+++ lib/grape-skylight/middleware.rb
@@ -3,19 +3,27 @@ require 'skylight'
 
 module Skylight
   module Grape
+    # Middleware to enable Skylight reporting with Grape.
     class Middleware < ::Grape::Middleware::Base
       def initialize(app)
         @app = app
       end
 
       def call(env)
-        request = env['api.endpoint'].routes.first
-        request_route = request.route_path.sub(/\(\.:format\)\z/, '')
-        # produces a string like: GET /hello/:name
-        name = "#{request.route_method} #{request_route}"
+        @app.call(env).tap do
+          # get the instrumenter instance, or skip
+          instrumenter = Skylight::Instrumenter.instance
+          next unless instrumenter
+          trace = instrumenter.current_trace
+          next unless trace
 
-        ::Skylight.trace(name) do
-          @app.call(env)
+          # produces a string to represent the route like: GET /hello/:name
+          request = env['api.endpoint'].routes.first
+          request_route = request.route_path.sub(/\(\.:format\)\z/, '')
+          name = "#{request.route_method} #{request_route}"
+
+          # set the endpoint to our route
+          trace.endpoint = name
         end
       end
     end
